eefde649a9db3118159e2aa71704dea1acc93b24
song lu <714628712@qq.com>
2017-08-02 10:36:34 +0800

    config

--- src/config/zmxy.php
+++ src/config/zmxy.php
@@ -5,12 +5,12 @@ return [
         "gatewayUrl" => "https://zmopenapi.zmxy.com.cn/openapi.do",
         //商户私钥文件
         "privateKeyFile" => "/var/www/test/app/Libarys/Zhima/keys/private_key.pem",
-        //芝麻公钥文件
+        //芝麻公钥文件,参考：https://b.zmxy.com.cn/technology/openDoc.htm?relInfo=RSA_INFO_DOC
         "zmPublicKeyFile" => "/var/www/test/app/Libarys/Zhima/keys/public_key.pem",
         //数据编码格式
         "charset" => "UTF-8",
         //芝麻分配给商户的 appId
-        "appId" => "1004070",
+        "appId" => "1000000",
     ],
 
 ];
